51a450e0191f11600d608e88268b842f72e71a74
Kreator Nusantara <93833445+kreatornusa@users.noreply.github.com>
2023-08-20 01:50:41 +0700

    Update copyright.js

--- javascript/copyright.js
+++ javascript/copyright.js
@@ -1,6 +1,6 @@
 var storedText;
 
-fetch('https://cdn.jsdelivr.net/gh/kreatornusa/wikichord/license/license.txt')
+fetch('https://cdn.jsdelivr.net/gh/kreatornusa/wikichord/license/copyright.txt')
   .then(function(response) {
     response.text().then(function(text) {
       storedText = text;
